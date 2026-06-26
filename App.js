738a801b228c694333d6149bb848176419a5d954
Bruno Otavio <brunootavio326@gmail.com>
2026-06-25 23:19:42 -0300

    chore: update package configuration and dependencies
    - Changed main entry point to expo/AppEntry.js in package.json
    - Updated react-native-reanimated to version 4.1.1 and added react-native-worklets
    - Added Babel plugins for improved compatibility
    - Adjusted tailwind.config.js to clean up content array and removed unused plugins
    - Modified tsconfig.json to allow JavaScript files and exclude node_modules

--- App.js
+++ App.js
@@ -1,9 +1,4 @@
 import "./src/styles/global.css";
-
-// Correção para o erro de Runtime do FormData:
-if (typeof global.FormData === 'undefined') {
-  global.FormData = require('react-native/Libraries/Network/FormData');
-}
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Home } from "./src/screens/home";
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {},
   },
-  plugins: [require('nativewind/tailwind/css')],
+  plugins: [],
   corePlugins: {
     preflight: false,
   },
-}
+};
